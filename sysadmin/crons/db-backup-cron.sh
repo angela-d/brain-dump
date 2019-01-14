724c961274e8e582116ca3de6aea0a71581363fd
angela <33108490+angela-d@users.noreply.github.com>
2019-01-13 19:06:34 -0600

    Add SSH key auth
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

--- sysadmin/crons/db-backup-cron.sh
+++ sysadmin/crons/db-backup-cron.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# path to your local repository
 LOCAL_REPO='/storage/db_backups'
+
+# specify which private key to use (the pubkey should already be in ~/.ssh/authorized_keys on the remote server)
+# as well as the fingerprint stored locally in known_hosts (do a manual pull the first time you set this up)
+# note that if you have a password on your key, it'll bungle this script up unless you add additional processing for the pw
+USEKEY='~/.ssh/your_private_key'
 
 # see if a vpn connection is active, i use openvpn
 if [ "$(pgrep -a openvpn)" = "" ];
 then
 
   cd "$LOCAL_REPO" && \
-  GITOUTPUT=$(git pull 2>&1) && \
+  ssh-agent bash -c "ssh-add $USEKEY; git pull 2>&1" && \
   cd && \
-  notify-send -i git-gui "Remote Database Backup" -u critical "$GITOUTPUT"
+  notify-send -i git-gui "Remote Database Backup" -u critical "Weekly backup complete, at $LOCAL_REPO"
 
 else
 
