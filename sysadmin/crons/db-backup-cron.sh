a090fac25b4f7d35f12a3f759f5777f8bbfb8511
angela <33108490+angela-d@users.noreply.github.com>
2019-01-13 19:29:36 -0600

    ssh-agent timeout threshold
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

--- sysadmin/crons/db-backup-cron.sh
+++ sysadmin/crons/db-backup-cron.sh
@@ -3,6 +3,9 @@
 # path to your local repository
 LOCAL_REPO='/storage/db_backups'
 
+# specify a timeout for ssh-agent to live in seconds; otherwise its forever
+TIMEOUT='150'
+
 # specify which private key to use (the pubkey should already be in ~/.ssh/authorized_keys on the remote server)
 # as well as the fingerprint stored locally in known_hosts (do a manual pull the first time you set this up)
 # note that if you have a password on your key, it'll bungle this script up unless you add additional processing for the pw
@@ -13,7 +16,8 @@ if [ "$(pgrep -a openvpn)" = "" ];
 then
 
   cd "$LOCAL_REPO" && \
-  ssh-agent bash -c "ssh-add $USEKEY; git pull 2>&1" && \
+  ssh-agent bash -c "ssh-add -t "$TIMEOUT" "$USEKEY"; git pull 2>&1" && \
+  ssh-agent -k \
   cd && \
   notify-send -i git-gui "Remote Database Backup" -u critical "Weekly backup complete, at $LOCAL_REPO"
 
