9d60933cf047bd7b9e60eec2932cabf1582fde5f
angela <33108490+angela-d@users.noreply.github.com>
2020-08-13 20:43:59 -0500

    Wayland script for autostart LED
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

---
+++ ux-stuff/keyboard-led-wayland.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# only root can write the /sys/class path
+if [ "$EUID" == 0 ];
+then
+  # get all scrolllock led ids
+  BRIGHTNESS_PATH=$(ls /sys/class/leds/input*\:\:scrolllock/brightness)
+
+  # loop through everything matching the brightness path and light em up
+  for scrollnum in $BRIGHTNESS_PATH
+  do
+    echo 1 > "$scrollnum"
+  done
+fi
