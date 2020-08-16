63a62493fea8ae61ed2cabcc4361604c5234c0dc
angela <33108490+angela-d@users.noreply.github.com>
2020-08-16 17:41:06 -0500

    Add terminal responses
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

--- ux-stuff/keyboard-led-wayland.sh
+++ ux-stuff/keyboard-led-wayland.sh
@@ -3,12 +3,16 @@
 # only root can write the /sys/class path
 if [ "$EUID" == 0 ];
 then
-  # get all scrolllock led ids
-  BRIGHTNESS_PATH=$(ls /sys/class/leds/input*\:\:scrolllock/brightness)
+# get all scrolllock led ids
+BRIGHTNESS_PATH=$(ls /sys/class/leds/input*\:\:scrolllock/brightness)
 
-  # loop through everything matching the brightness path and light em up
-  for scrollnum in $BRIGHTNESS_PATH
-  do
-    echo 1 > "$scrollnum"
-  done
+# loop through everything matching the brightness path and light em up
+for scrollnum in $BRIGHTNESS_PATH
+do
+  echo 1 > "$scrollnum"
+  echo "$scrollnum modified"
+done
+else
+ echo -e "$USER requires sudo permissions to: $0\\n\\tCommon problems:"
+ echo -e "\\t  - Ordering in visudo command"
 fi
